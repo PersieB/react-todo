a116675a607c4bf47e4c435af6badb03686999d7
PersieB <persiebrown285@gmail.com>
2023-05-22 20:07:34 +0000

    created check feature on tasks

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,8 +1,33 @@
+
+import { useState } from 'react';
+import styles from './TaskItem.module.css';
+import {CheckIcon} from '@heroicons/react/24/outline';
+
 const TaskItem = ({task}) =>{
+    const [isChecked, setIsChecked] = useState(task.echecked);
+
+    const handleCheckBoyChange = (e) => {
+        setIsChecked(!isChecked);
+    }
     return(
-        <div>
-            {task.name}
-        </div>
+        <li className={styles.task}>
+            <div className={styles["task-group"]}>
+                <input type="checkbox" 
+                className={styles.checkbox}
+                checked={isChecked}
+                name={task.name}
+                id={task.id}
+                onChange={handleCheckBoyChange}
+                 />
+                <label htmlFor={task.id}
+                className={styles.label}> 
+                    {task.name}
+                <p className={styles.checkmark}>
+                    <CheckIcon strokeWidth={2} width={24} height={24} />
+                </p>
+                </label>
+            </div>
+        </li>
     )
 }
 

fa74acba03404d0439de81af35e0199bafaa9173
PersieB <persiebrown285@gmail.com>
2023-05-22 18:33:21 +0000

    creating custom form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 
+// custom component
+import CustomForm from './components/CustomForm'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div className='App'>
-
+      <div className='container'>
+        <header>
+          <h1> My Todo Tasks </h1>
+        </header>
+        <CustomForm />
       </div>
 
   )
--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
+import { PlusIcon } from '@heroicons/react/24/solid'
 
 const CustomForm = () => {
+    const [task, setTask] = useState("");
+    
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        console.log(e);
+        setTask("");
     }
     return (
         <form className="todo" onSubmit = {handleFormSubmit}>
             <div className="wrapper">
-
-                
+                <input type="text" id = "task" className="input" 
+                value = {task}
+                onChange={(e) => setTask(e.target.value)}
+                required
+                autoFocus
+                maxLength={70}
+                placeholder="Enter Task"
+                />
+                <label htmlFor="task" className="label">Enter Task</label>
             </div>
-
+            <button className="btn"
+            aria-label="Add Task"
+            type="submit">
+                <PlusIcon />
+            </button>
         </form>
     )
 }
